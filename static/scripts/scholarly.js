6215fad8907f3e311c9f9c99bc08323ef79644cc
Weeblizer-1 <131916107+Weeblizer-1@users.noreply.github.com>
2023-11-28 15:45:44 -0500

    Update scholarly.js

--- static/scripts/scholarly.js
+++ static/scripts/scholarly.js
@@ -32,7 +32,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        location.replace("https://www.neshaminy.instructure.com/dashboard")
+        location.replace("https://neshaminy.instructure.com/dashboard")
     }
 }
 
